aa74c64a30ca004d7169b5d3de48a8d3beb92007
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 12:13:04 +0100

    Added import to main for fontAwesome

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,7 +11,10 @@ import "./assets/main.css";
 
 const app = createApp(App);
 
+library.add(fas);
+
 app.use(createPinia());
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
