8ae968834b6ca81d2239f996248e5a4776914178
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 12:15:40 +0100

    created details view

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="detail">
+    <h1>This is a pokemon details view</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .details {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
