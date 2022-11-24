c83ce6a9a2dc1cfb72433878944988a8df9e445f
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 16:22:42 +0100

    added created elements to list element

--- src/components/ThePokemonList.vue
+++ src/components/ThePokemonList.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
-import ThePokemonCard from "@/components/ThePokemonCard.vue";
+import PokemonCard from "@/components/elements/PokemonCard.vue";
+import ListHeader from "./elements/ListHeader.vue";
+import Input from "@/components/elements/Input.vue";
+import ListNavButton from "./elements/NavButton.vue";
 </script>
 
 <template>
-  <ThePokemonCard />
+  <ListHeader />
+  <Input name="Pokemon zoeken" value="" placeholder="Pokemon zoeken" />
+  <div class="button-container"><ListNavButton /> <ListNavButton /></div>
+  <PokemonCard />
 </template>
+
+<script lang="ts"></script>
+
+<style scoped>
+.button-container {
+  display: flex;
+  gap: 9px;
+  margin-top: 19px;
+  margin-bottom: 20px;
+}
+</style>
