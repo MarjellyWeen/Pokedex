f734fddb0ac2349823cad388a4eeb14e0773fcb9
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 20:21:18 +0100

    created team view and list

---
+++ src/components/TheTeamList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PokemonCard from "@/components/elements/PokemonCard.vue";
+import TheHeader from "./TheHeader.vue";
+import CustomInput from "@/components/elements/Input.vue";
+</script>
+
+<template>
+  <TheHeader title="Team" />
+  <CustomInput name="Pokemon zoeken" placeholder="Pokemon zoeken" />
+
+  <PokemonCard />
+</template>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheTeamList from "@/components/TheTeamList.vue";
+</script>
+
+<template>
+  <div class="team"><TheTeamList /></div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .team {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
