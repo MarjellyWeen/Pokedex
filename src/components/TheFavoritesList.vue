cafe76d300e808ad5f2b457255139015e9718d0a
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 20:21:58 +0100

    created and adjusted favorites view and list

---
+++ src/components/TheFavoritesList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PokemonCard from "@/components/elements/PokemonCard.vue";
+import TheHeader from "./TheHeader.vue";
+import CustomInput from "@/components/elements/Input.vue";
+</script>
+
+<template>
+  <TheHeader title="Favorites" />
+  <CustomInput name="Pokemon zoeken" placeholder="Pokemon zoeken" />
+
+  <PokemonCard />
+</template>
+
+<style scoped>
+.button-container {
+  display: flex;
+  gap: 9px;
+  margin-top: 19px;
+  margin-bottom: 20px;
+}
+</style>
--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,7 +1,9 @@
+<script setup lang="ts">
+import TheFavoritesList from "@/components/TheFavoritesList.vue";
+</script>
+
 <template>
-  <div class="favorites">
-    <h1>This is a favorites view</h1>
-  </div>
+  <TheFavoritesList />
 </template>
 
 <style>
@@ -10,6 +12,7 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    background: linear-gradient(109.73deg, #46469c 0%, #7e32e0 100%);
   }
 }
 </style>
