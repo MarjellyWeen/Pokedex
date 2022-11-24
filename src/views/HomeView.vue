2dbe4455821a25f31432cc5fc81abbb2c23ec18c
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 16:21:34 +0100

    Adjusted home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheHeader from "@/components/TheHeader.vue";
 import ThePokemonList from "@/components/ThePokemonList.vue";
 </script>
 
 <template>
   <main>
-    <TheHeader />
     <ThePokemonList />
   </main>
 </template>
