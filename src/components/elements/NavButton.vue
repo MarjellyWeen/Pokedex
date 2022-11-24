5b8f84426abde6974af795809710acaa72e0e2d5
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 16:22:05 +0100

    created navButton element

---
+++ src/components/elements/NavButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="button">
+    <h2>Favorites</h2>
+  </div>
+</template>
+
+<style scoped>
+.button {
+  width: 167px;
+  height: 100px;
+  border-radius: 10px;
+  background: linear-gradient(109.73deg, #46469c 0%, #7e32e0 100%);
+}
+.button .purple {
+  background: linear-gradient(109.73deg, #46469c 0%, #7e32e0 100%);
+}
+.button .blue {
+  background: linear-gradient(109.73deg, #65cb9a 0%, #15d0dc 100%);
+}
+</style>
