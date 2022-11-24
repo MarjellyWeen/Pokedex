47919489cf707e91d5b497bb2f8de3e15a59fd17
marjellyween <marjolijn.knops@gmail.com>
2022-11-24 12:16:57 +0100

    created header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <h1>Pok&#233;dex</h1>
+    <div>
+      <font-awesome-icon icon="fa-solid fa-filter" />
+      <font-awesome-icon icon="fa-solid fa-arrow-right-arrow-left" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  height: 150px;
+  justify-content: space-between;
+  align-items: center;
+  top: "calc(env(safe-area-inset-top) + 10px)";
+}
+</style>
