7091c6441bf4fcbd81026a8916fd1be7ad3e7af4
marjellyween <marjolijn.knops@gmail.com>
2022-11-25 21:33:22 +0100

    created pokemon details component

---
+++ src/components/ThePokemonDetails.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import TheHeader from "./TheHeader.vue";
+import PokemonDetailsImage from "@/components/elements/PokemonDetailsImage.vue";
+import DetailsCard from "./elements/DetailsCard.vue";
+</script>
+
+<template>
+  <main>
+    <TheHeader title="Venusaur" is-light-color is-details-view />
+    <PokemonDetailsImage />
+    <DetailsCard
+      name="About"
+      description="Vanaf de dag dat deze Pokémon wordt geboren, zit er een plantenzaadje op zijn rug. Het zaad wordt langzaam groter."
+    />
+    <DetailsCard name="Stats" />
+    <DetailsCard name="Evolution" />
+  </main>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped>
+main {
+  background: linear-gradient(180deg, #7ecd8b 0%, #89e2b3 100%);
+}
+</style>
