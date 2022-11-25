b8b6cec8c81b8a84c33f5e2405f8d0ad1fa3c6ba
marjellyween <marjolijn.knops@gmail.com>
2022-11-25 21:17:25 +0100

    added background color to teamview

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -3,10 +3,13 @@ import TheTeamList from "@/components/TheTeamList.vue";
 </script>
 
 <template>
-  <div class="team"><TheTeamList /></div>
+  <main class="team"><TheTeamList /></main>
 </template>
 
 <style>
+.team {
+  background: linear-gradient(109.73deg, #46469c 0%, #7e32e0 100%);
+}
 @media (min-width: 1024px) {
   .team {
     min-height: 100vh;
