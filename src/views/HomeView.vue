fcf7a13f0cab8483ab53e65f892bb7e01b3ddc7b
marjellyween <marjolijn.knops@gmail.com>
2022-11-25 21:37:00 +0100

    Created first fetch of pokemon data in list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import ThePokemonList from "@/components/ThePokemonList.vue";
+import { toRefs } from "vue";
+
+import { usePokemonStore } from "../stores/pokemon";
+
+const { pokemons } = toRefs(usePokemonStore());
+const { fetchPokemons } = usePokemonStore();
+
+fetchPokemons();
+
+console.log(pokemons);
 </script>
 
 <template>
