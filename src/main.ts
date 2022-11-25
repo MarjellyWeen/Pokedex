44e87b70dfc8bb3940def6b0f8c46e845b5a4319
marjellyween <marjolijn.knops@gmail.com>
2022-11-25 21:18:16 +0100

    add import to far icons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from "pinia";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import VueEasyLightbox from "vue-easy-lightbox";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,8 +13,9 @@ import "./assets/main.css";
 
 const app = createApp(App);
 
-library.add(fas);
+library.add(fas, far);
 
+app.use(VueEasyLightbox);
 app.use(createPinia());
 app.use(router);
 app.component("font-awesome-icon", FontAwesomeIcon);
